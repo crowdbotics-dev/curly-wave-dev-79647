071ae2473c60028e48c4fe6ce365a618d0e760ef
Crowdbotics <engineering@crowdbotics.com>
2023-04-28 09:37:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-wave-dev-79647",
+  url: "https://curly-wave-dev-79647.botics.co",
+  api: "https://curly-wave-dev-79647.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
